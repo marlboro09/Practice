02b67a81c470f87b6158ea5598ba129e187a2c85
marlboro09 <taebong8989@gmail.com>
2024-05-04 01:12:02 +0900

    if와 switch 연습

---
+++ src/iftest/Main.java
@@ -0,0 +1,13 @@
+package iftest;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean condition = true;
+
+        if(condition) {
+            System.out.println("조건은 참");//조건이 참일때 출력
+        } else {
+            System.out.println("조건은 거짓");//조건이 거짓일때 출력
+        }
+    }
+}
---
+++ src/switchtest/Main.java
@@ -0,0 +1,20 @@
+package switchtest;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 2;
+
+        switch(num) {
+            case 1:
+                System.out.println("결과는 1");
+                break;
+            case 2:
+            case 3:
+                System.out.println("결과는 2 또는 3");//case2에 break가 없기 때문에 3번이 출력 된다
+                break;
+            default:
+                System.out.println("결과는 1,2,3이 아니다");//해당되는것이 없을 때 출력
+                break;
+        }
+    }
+}
