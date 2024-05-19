7d646f72f6ac16c071468b19f1e8baccaa92559d
marlboro09 <taebong8989@gmail.com>
2024-05-20 01:04:40 +0900

    내부 생성자 연습

---
+++ src/num6/inneclass/InnerTest.java
@@ -0,0 +1,32 @@
+package num6.inneclass;
+
+/**
+ * Outer클래스와 Inner클래스 선언
+ */
+class Outer {
+	static int a = 10;
+	int b = 20;
+	void f(){
+		System.out.println("안녕하세요");
+	}
+	class Inner {
+		int c = 30;
+		public void g() {
+			b=100;
+			f();
+			System.out.println(a+" "+c);
+		}
+	}
+}
+
+
+public class InnerTest {
+	public static void main(String[] args) {
+		System.out.println("Outer에서 Inner 객체 선언하기");
+		Outer out = new Outer();
+		Outer.Inner in = out.new Inner();
+		in.g();	//Inner class의 g()메서드 호출하여 System.out.println(a+" "+c);실행
+		System.out.println(out.b);
+		System.out.println();
+	}
+}
